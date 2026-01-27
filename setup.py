3f5ff64eeec54425f8ad5551b205129e4b8556f3
alexandr_dragunkin <alexandr69@gmail.com>
2026-01-27 11:58:36 +0300

    add: relise

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="stl2wrl",
+    version="1.0.0",
+    author="Bradley Boccuzzi",
+    author_email="bradleyboccuzzi@example.com",
+    description="A tool to convert STL files to VRML/WRL format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/stl2wrl",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Manufacturing",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+    ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "stl2wrl=stl2wrl:main",
+        ],
+    },
+)
