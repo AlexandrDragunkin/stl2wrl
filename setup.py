5d4ed76b00c33db4ec21b020c68322c48edc371c
alexandr_dragunkin <alexandr69@gmail.com>
2026-01-29 13:51:51 +0300

    edit ver

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="stl2wrl",
-    version="1.1.0",
+    version="1.1.0a",
     author="Bradley Boccuzzi",
     author_email="bradleyboccuzzi@example.com",
     description="A tool to convert STL files to VRML/WRL format",
